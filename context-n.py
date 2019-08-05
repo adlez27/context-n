fcc612e542f21b38b6e24b764d2b1af9434894f9
Kiyoteru Lofe Adlez Dystopia <meggiebeltran@gmail.com>
2019-08-05 16:43:21 +0800

    Fixed compatibility with VCV

--- context-n.py
+++ context-n.py
@@ -7,7 +7,6 @@ config.read('config.ini')
 ust = configparser.RawConfigParser(allow_no_value=True)
 ust.optionxform = lambda option: option
 ust.read(sys.argv[1])
-# ust.read('test.ust')
 
 if ('#NEXT' in ust):
     first_note_dex = int(ust.sections()[3][-4:])
@@ -31,17 +30,14 @@ for note in ust.sections():
             if('ん' in ust['#'+current_note_dex]['Lyric']):
                 for next_kana in config['mapping']:
                     if(next_kana in ust['#'+next_note_dex]['Lyric']):
-                        ust['#'+current_note_dex]['Lyric'] = config['mapping'].get(next_kana)
-                        break
+                        ust['#'+current_note_dex]['Lyric'] = ust['#'+current_note_dex]['Lyric'].replace('ん',config['mapping'].get(next_kana))
             break
         else:
             if('ん' in ust['#'+current_note_dex]['Lyric']):
                 for next_kana in config['mapping']:
                     if(next_kana in ust['#'+next_note_dex]['Lyric']):
-                        ust['#'+current_note_dex]['Lyric'] = config['mapping'].get(next_kana)
-                        break
+                        ust['#'+current_note_dex]['Lyric'] = ust['#'+current_note_dex]['Lyric'].replace('ん',config['mapping'].get(next_kana))
             note_count += 1
 
-# with open('test.ust', 'w') as output:
 with open(sys.argv[1], 'w') as output:
     ust.write(output, space_around_delimiters=False)
