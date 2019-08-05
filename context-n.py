1f43dfc1875981a4dc289f39237950b0e8b581c3
Kiyoteru Lofe Adlez Dystopia <meggiebeltran@gmail.com>
2019-08-05 16:28:57 +0800

    Add files via upload

---
+++ context-n.py
@@ -0,0 +1,47 @@
+import sys
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+ust = configparser.RawConfigParser(allow_no_value=True)
+ust.optionxform = lambda option: option
+ust.read(sys.argv[1])
+# ust.read('test.ust')
+
+if ('#NEXT' in ust):
+    first_note_dex = int(ust.sections()[3][-4:])
+    last_note_count = len(ust.sections())-5
+    partial = True
+else:
+    first_note_dex = int(ust.sections()[2][-4:])
+    last_note_count = len(ust.sections())-4
+    partial = False
+
+note_count = 0
+
+for note in ust.sections():
+    if ((note != '#VERSION') and (note != '#SETTING') and (note != '#PREV') and (note != '#NEXT') and (note !='#TRACKEND')):
+        current_note_dex = str(first_note_dex + note_count).zfill(4)
+        next_note_dex = str(first_note_dex + note_count + 1).zfill(4)
+
+        if(note_count == last_note_count):
+            if(partial):
+                next_note_dex = 'NEXT'
+            if('ん' in ust['#'+current_note_dex]['Lyric']):
+                for next_kana in config['mapping']:
+                    if(next_kana in ust['#'+next_note_dex]['Lyric']):
+                        ust['#'+current_note_dex]['Lyric'] = config['mapping'].get(next_kana)
+                        break
+            break
+        else:
+            if('ん' in ust['#'+current_note_dex]['Lyric']):
+                for next_kana in config['mapping']:
+                    if(next_kana in ust['#'+next_note_dex]['Lyric']):
+                        ust['#'+current_note_dex]['Lyric'] = config['mapping'].get(next_kana)
+                        break
+            note_count += 1
+
+# with open('test.ust', 'w') as output:
+with open(sys.argv[1], 'w') as output:
+    ust.write(output, space_around_delimiters=False)
