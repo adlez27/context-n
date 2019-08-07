bc01be6b09a728ca3a3d35354b24b05437d347c8
Kiyoteru Lofe Adlez Dystopia <meggiebeltran@gmail.com>
2019-08-07 17:06:10 +0800

    Romaji compatibility

--- context-n.py
+++ context-n.py
@@ -1,16 +1,20 @@
 import sys
 import os.path
+import shutil
 import configparser
 
 ust = configparser.RawConfigParser(allow_no_value=True)
 ust.optionxform = lambda option: option
 ust.read(sys.argv[1])
 
-config = configparser.ConfigParser()
+config = configparser.RawConfigParser(allow_no_value=True)
+config.optionxform = lambda option: option
 if (os.path.exists(ust['#SETTING']['VoiceDir'] + "\\context-n.ini")):
     config.read(ust['#SETTING']['VoiceDir'] + "\\context-n.ini")
 else:
-    config.read('config.ini')
+    shutil.copyfile('default.ini', ust['#SETTING']['VoiceDir'] + "\\context-n.ini")
+    config.read(ust['#SETTING']['VoiceDir'] + "\\context-n.ini")
+    # TODO: GUI opens to create new per-vb settings
 
 if ('#NEXT' in ust):
     first_note_dex = int(ust.sections()[3][-4:])
@@ -21,8 +25,27 @@ else:
     last_note_count = len(ust.sections())-4
     partial = False
 
-note_count = 0
+def edit(current,next):
+    if(config['settings'].getboolean('romaji')):
+        if('n' in ust['#'+current]['Lyric']):
+            valid = True
+            for ex in config['exceptions']:
+                if (ex in ust['#'+current]['Lyric']):
+                    valid = False
+                    break
+            if(valid):
+                for next_lyric in config['mapping']:
+                    if(next_lyric in ust['#'+next]['Lyric']):
+                        ust['#'+current]['Lyric'] = ust['#'+current]['Lyric'].replace('n',config['mapping'].get(next_lyric))
+                        break
+    else:
+        if('ん' in ust['#'+current]['Lyric']):
+            for next_lyric in config['mapping']:
+                if(next_lyric in ust['#'+next]['Lyric']):
+                    ust['#'+current]['Lyric'] = ust['#'+current]['Lyric'].replace('ん',config['mapping'].get(next_lyric))
+                    break
 
+note_count = 0
 for note in ust.sections():
     if ((note != '#VERSION') and (note != '#SETTING') and (note != '#PREV') and (note != '#NEXT') and (note !='#TRACKEND')):
         current_note_dex = str(first_note_dex + note_count).zfill(4)
@@ -31,16 +54,10 @@ for note in ust.sections():
         if(note_count == last_note_count):
             if(partial):
                 next_note_dex = 'NEXT'
-            if('ん' in ust['#'+current_note_dex]['Lyric']):
-                for next_kana in config['mapping']:
-                    if(next_kana in ust['#'+next_note_dex]['Lyric']):
-                        ust['#'+current_note_dex]['Lyric'] = ust['#'+current_note_dex]['Lyric'].replace('ん',config['mapping'].get(next_kana))
+            edit(current_note_dex, next_note_dex)
             break
         else:
-            if('ん' in ust['#'+current_note_dex]['Lyric']):
-                for next_kana in config['mapping']:
-                    if(next_kana in ust['#'+next_note_dex]['Lyric']):
-                        ust['#'+current_note_dex]['Lyric'] = ust['#'+current_note_dex]['Lyric'].replace('ん',config['mapping'].get(next_kana))
+            edit(current_note_dex, next_note_dex)
             note_count += 1
 
 with open(sys.argv[1], 'w') as output:
